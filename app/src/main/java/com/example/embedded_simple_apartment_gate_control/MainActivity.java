64c6e44176b510b6cba02da254e16211e76cbebd
hoho00 <jhoya963@gmail.com>
2020-11-05 00:08:26 +0900

    first commit

---
+++ app/src/main/java/com/example/embedded_simple_apartment_gate_control/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.embedded_simple_apartment_gate_control;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
